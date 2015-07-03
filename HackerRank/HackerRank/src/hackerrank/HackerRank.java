9e1d4d6f1c8f115187af3384af4322f70db80e54
Rajesh Surana <rsurana@asu.edu>
2015-07-02 18:24:36 -0700

    HackerRank Project Created

---
+++ HackerRank/HackerRank/src/hackerrank/HackerRank.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hackerrank;
+import java.util.Scanner;
+/**
+ *
+ * @author Rajesh
+ */
+public class HackerRank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //HackerRank hr = new HackerRank();
+        
+        // Test diagonalDiff
+        System.out.println(DiagonalDiff.diagonalDifference());
+    }
+    
+}

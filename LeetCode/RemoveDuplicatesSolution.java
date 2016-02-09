b2ebb81c7fe80a390dcc200741dee8097cc696d3
Rajesh Surana <rsurana@asu.edu>
2016-02-09 08:29:52 -0700

    Remove Duplicates from Sorted Array
    Given a sorted array, remove the duplicates in place such that each
    element appear only once and return the new length.

---
+++ LeetCode/RemoveDuplicatesSolution.java
@@ -0,0 +1,22 @@
+/*
+Remove Duplicates from Sorted Array
+Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+For example,
+Given input array nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
+*/
+public class RemoveDuplicatesSolution {
+    public int removeDuplicates(int[] nums) {
+        int j=0;
+        for(int i=1; i<nums.length; i++){
+            if(nums[j] != nums[i]){
+                nums[++j] = nums[i];
+            }
+        }
+        return ++j;
+    }
+}

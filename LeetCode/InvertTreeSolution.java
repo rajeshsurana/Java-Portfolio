8fd5b0338f8f83c61f322514055abcde674f260a
Rajesh Surana <rsurana@asu.edu>
2016-02-11 09:30:48 -0700

    Invert Binary Tree
    Invert a binary tree.

---
+++ LeetCode/InvertTreeSolution.java
@@ -0,0 +1,24 @@
+/*
+226. Invert Binary Tree
+Invert a binary tree.
+*/
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class InvertTreeSolution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = invertTree(root.right);
+        root.right = invertTree(temp);
+        return root;
+    }
+}
